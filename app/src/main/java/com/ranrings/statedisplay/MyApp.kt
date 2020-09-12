86b10a8be6fb783eaf8890e7c95f41c879bf4eb9
Sushobh Nadiger <sushobh.nadiger@paynearby.in>
2020-09-12 15:28:19 +0530

    OK

---
+++ app/src/main/java/com/ranrings/statedisplay/MyApp.kt
@@ -0,0 +1,28 @@
+package com.ranrings.statedisplay
+
+
+import android.app.Application
+
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+
+class MyApp : Application() {
+
+
+    companion object {
+        lateinit var appContext : Application
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        appContext = this
+        startKoin {
+            androidLogger(Level.NONE)
+            androidContext(this@MyApp)
+            modules(appModule)
+        }
+
+    }
+}
